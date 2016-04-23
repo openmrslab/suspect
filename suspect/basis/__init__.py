afdcda466a8ac027a544f473f6654a2a1af65257
bennyrowland <bennyrowland@mac.com>
2016-04-23 16:20:27 -0400

    fixed Gaussian peak generation

--- suspect/basis/__init__.py
+++ suspect/basis/__init__.py
@@ -5,8 +5,9 @@ def gaussian(time_axis, frequency, phase, fwhm):
     oscillatory_term = numpy.exp(2j * numpy.pi * (frequency * time_axis + phase))
     damping = numpy.exp(-time_axis ** 2 / 4 * numpy.pi ** 2 / numpy.log(2) * fwhm ** 2)
     fid = oscillatory_term * damping
+    fid[0] /= 2.0
     # normalise the fid so the peak has area 1
-    return fid / len(time_axis)
+    return fid * (time_axis[1] - time_axis[0]) * 2.0
 
 
 def lorentzian(time_axis, frequency, phase, fwhm):
