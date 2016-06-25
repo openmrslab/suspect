70c8ae2cbfcfa25abf7e5684522f4a975eb80fd5
bennyrowland <bennyrowland@mac.com>
2016-06-25 13:18:39 -0400

    modified order of dependencies in setup.py for readthedocs

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
         ],
-        install_requires=['numpy', 'pywavelets', 'scipy', 'lmfit', 'pydicom'],
+        install_requires=['pywavelets', 'scipy', 'numpy', 'lmfit', 'pydicom'],
         test_requires=['pytest', 'mock']
 )
