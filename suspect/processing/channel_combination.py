499e4c97a56adf9895ce103a182e90a6a0148c91
bennyrowland <bennyrowland@mac.com>
2016-08-30 16:18:59 -0400

    fixed normalisation of svd channel weights calculation

--- suspect/processing/channel_combination.py
+++ suspect/processing/channel_combination.py
@@ -2,7 +2,7 @@ import numpy
 
 
 def svd_weighting(data):
-    p, _, _ = numpy.linalg.svd(data, full_matrices=False)
+    p, s, _ = numpy.linalg.svd(data, full_matrices=False)
     channel_weights = p[:, 0].conjugate()
 
-    return channel_weights / numpy.sum(channel_weights)
+    return channel_weights / numpy.sum(numpy.abs(channel_weights)) / s[0]
