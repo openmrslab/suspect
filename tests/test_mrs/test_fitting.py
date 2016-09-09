1f9a8d6437d927470a5670737741308271b0dbb3
bennyrowland <bennyrowland@mac.com>
2016-09-09 19:26:54 -0400

    fixed errors in singlet fitting and added first unit test

--- tests/test_mrs/test_fitting.py
+++ tests/test_mrs/test_fitting.py
@@ -6,56 +6,26 @@ import numpy
 
 def test_gaussian():
     time_axis = numpy.arange(0, 0.512, 5e-4)
-    fid = basis.gaussian(time_axis, 0, 0, 50.0)
+    fid = basis.gaussian(time_axis, 0, 0, 50.0) + 0.00001 * (numpy.random.rand(1024) - 0.5)
     data = MRSData(fid, 5e-4, 123)
-    model = singlet.Model({
-        "g1": {
-            "amplitude": 1.0,
-            "fwhm": 50.0,
+    model = {
+        "phase0": 0,
+        "phase1": 0,
+        "pcr": {
+            "amplitude": 1,
+            "width": {
+                "value": 45,
+                "min": 42,
+                "max": 55
+            },
             "phase": "0",
-            "frequency": 0.0
+            "frequency": 0
         }
-    })
-    fitting_result = model.fit(data)
-    numpy.testing.assert_allclose(fitting_result["params"]["g1"]["fwhm"], 50.0)
-    numpy.testing.assert_allclose(fitting_result["params"]["g1"]["amplitude"], 1.0)
-    numpy.testing.assert_allclose(fitting_result["params"]["g1"]["frequency"], 0.0, atol=1e-7)
+    }
+    fitting_result = singlet.fit(data, model)
 
-    numpy.testing.assert_allclose(fitting_result["fit"], fid)
+    numpy.testing.assert_allclose(fitting_result["model"]["pcr"]["width"], 50.0, rtol=1e-2)
+    numpy.testing.assert_allclose(fitting_result["model"]["pcr"]["amplitude"], 1.0, rtol=2e-2)
+    numpy.testing.assert_allclose(fitting_result["model"]["pcr"]["frequency"], 0.0, atol=1e-2)
 
-
-# def test_multiple_gaussians():
-#     time_axis = numpy.arange(0, 0.512, 5e-4)
-#     g1 = basis.gaussian(time_axis, 0, 0, 50.0)
-#     g2 = basis.gaussian(time_axis, 250, 0, 40.0)
-#     fid = g1 + g2
-#     data = MRSData(fid, 5e-4, 123)
-#     model = singlet.Model({
-#         "g1": {
-#             "amplitude": 1.0,
-#             "fwhm": 50.0,
-#             "phase": "0",
-#             "frequency": {"value": 0.0, "min": -50, "max": 50}
-#         },
-#         "g2": {
-#             "amplitude": "g1amplitude",
-#             "fwhm": 45,
-#             "phase": "0",
-#             "frequency": 230
-#         }
-#     })
-#
-#     fitting_result = model.fit(data)
-#     params = fitting_result["params"]
-#
-#     print(params["g1"])
-#     numpy.testing.assert_allclose(params["g1"]["fwhm"], 50.0)
-#     numpy.testing.assert_allclose(params["g1"]["amplitude"], 1.0)
-#     numpy.testing.assert_allclose(params["g1"]["frequency"], 0.0, atol=1e-7)
-#     numpy.testing.assert_allclose(params["g2"]["fwhm"], 40.0)
-#     numpy.testing.assert_allclose(params["g2"]["amplitude"], 1.0)
-#     numpy.testing.assert_allclose(params["g2"]["frequency"], 250.0)
-#
-#     numpy.testing.assert_allclose(fitting_result["fit"], fid)
-#     numpy.testing.assert_allclose(fitting_result["fit_components"]["g1"], g1)
-#     numpy.testing.assert_allclose(fitting_result["fit_components"]["g2"], g2)
+    numpy.testing.assert_allclose(fitting_result["fit"], fid, atol=0.001)
