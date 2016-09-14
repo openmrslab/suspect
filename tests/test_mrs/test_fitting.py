56bb1d8c043d64353b0b81f1610229a7ce6f8cb9
lasyasreepada <lasya.sreepada@gmail.com>
2016-09-14 10:51:02 -0400

    Modified the files suspect/fitting/singlet.py and tests/test_mrs/test_fitting in order to improve existing code and write a more comprehensive test

--- tests/test_mrs/test_fitting.py
+++ tests/test_mrs/test_fitting.py
@@ -3,12 +3,13 @@ from suspect import basis, MRSData
 
 import numpy
 
-
 def test_gaussian():
     time_axis = numpy.arange(0, 0.512, 5e-4)
     fid = basis.gaussian(time_axis, 0, 0, 50.0) + 0.00001 * (numpy.random.rand(1024) - 0.5)
     data = MRSData(fid, 5e-4, 123)
-    model = {
+
+    #Original test with all parameters passed in; correct data types; integer values
+    model1 = {
         "phase0": 0,
         "phase1": 0,
         "pcr": {
@@ -22,10 +23,100 @@ def test_gaussian():
             "frequency": 0
         }
     }
-    fitting_result = singlet.fit(data, model)
 
-    numpy.testing.assert_allclose(fitting_result["model"]["pcr"]["width"], 50.0, rtol=1e-2)
-    numpy.testing.assert_allclose(fitting_result["model"]["pcr"]["amplitude"], 1.0, rtol=2e-2)
-    numpy.testing.assert_allclose(fitting_result["model"]["pcr"]["frequency"], 0.0, atol=1e-2)
+    #Floating point values; invalid key added to width dict, to test whether KeyError is raised
+    model2 = {
+        "phase0": 0.0,
+        "phase1": 0.0,
+        "pcr": {
+            "amplitude": 1.0,
+            "width": {
+                "value": 45.0,
+                "min": 42.0,
+                "max": 55.0,
+                "avg": 47
+            },
+            "phase": "0",
+            "frequency": 0.0
+        }
+    }
 
-    numpy.testing.assert_allclose(fitting_result["fit"], fid, atol=0.001)
+    #No width value passed in, to test whether KeyError is raised
+    model3 = {
+        "phase0": 0,
+        "phase1": 0,
+        "pcr": {
+            "amplitude": 1,
+            "width": {
+                #"value": 45,
+                "min": 42,
+                "max": 55,
+
+            },
+            "phase": "0",
+            "frequency": 0
+        }
+    }
+
+    #No phase value passed in, to test whether phase is fixed to 0 by default
+    model4 = {
+        "phase0": 0,
+        "phase1": 0,
+        "pcr": {
+            "amplitude": 1,
+            "width": {
+                "value": 45,
+                "min": 42,
+                "max": 55,
+            },
+            # "phase": "0",
+            "frequency": 0
+        }
+    }
+
+    #Str value supplied for phase0 and phase1, to test whether TypeError is raised
+    model5 = {
+        "phase0": str,
+        "phase1": str,
+        "pcr": {
+            "amplitude": 1.0,
+            "width": {
+                "value": 45.0,
+                "min": 42.0,
+                "max": 55.0,
+            },
+            "phase": "0",
+            "frequency": 0.0
+        }
+    }
+
+    # Str value supplied for amplitude, to test whether TypeError is raised
+    model6 = {
+        "phase0": 0.0,
+        "phase1": 0.0,
+        "pcr": {
+            "amplitude": str,
+            "width": {
+                "value": 45.0,
+                "min": 42.0,
+                "max": 55.0,
+            },
+            "phase": "0",
+            "frequency": 0.0
+        }
+    }
+
+    set_of_models = [model1, model2, model3, model4, model5, model6]
+
+    for model in set_of_models:
+        try:
+            fitting_result = singlet.fit(data, model)
+
+            numpy.testing.assert_allclose(fitting_result["model"]["pcr"]["width"], 50.0, rtol=1e-2)
+            numpy.testing.assert_allclose(fitting_result["model"]["pcr"]["amplitude"], 1.0, rtol=2e-2)
+            numpy.testing.assert_allclose(fitting_result["model"]["pcr"]["frequency"], 0.0, atol=1e-2)
+
+            numpy.testing.assert_allclose(fitting_result["fit"], fid, atol=0.001)
+
+        except(TypeError, KeyError, ReferenceError):
+            pass
